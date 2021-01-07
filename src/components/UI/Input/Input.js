ed25aa6d7442fd8a6475bff0447938a1ca992e04
Forest <azureforestryan@gmail.com>
2021-01-07 08:46:15 +0000

    Form input two way binding

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -3,17 +3,16 @@ import classes from './Input.module.css'
 
 const input = (props) => {
     let inputElement = null;
-    console.log(props.elementConfig.options)
     switch (props.elementType) {
         case ('input'):
-            inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value}/>;
+            inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value} onChange={props.changed}/>;
             break;
         case ('textarea'):
-            inputElement = <textarea className={classes.InputElement} {...props.elementConfig} value={props.value}/>;
+            inputElement = <textarea className={classes.InputElement} {...props.elementConfig} value={props.value} onChange={props.changed}/>;
             break;
         case ('select'):
             inputElement = (
-                <select className={classes.InputElement} value={props.value}>
+                <select className={classes.InputElement} value={props.value} onChange={props.changed}>
                     {props.elementConfig.options.map(option => (
                         <option key={option.value} value={option.value}>{option.displayValue}</option>
                     ))}
@@ -21,7 +20,7 @@ const input = (props) => {
             );
             break;
         default:
-            inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value}/>;
+            inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value} onChange={props.changed}/>;
     }
 
     return (
