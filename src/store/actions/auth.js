441cf90d107c532aedbf7bb973f83ce1550627f9
Forest1027 <azureforestryan@gmail.com>
2021-01-20 08:58:47 +0000

    get token from backend

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 import * as actionTypes from './actionsTypes';
 
 export const authStart = () => {
@@ -23,5 +25,20 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
     return dispatch => {
         dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }
+        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAVnERqLQzwbTV0Zg9x133avl_bB5TG_9Y', authData)
+            .then(response => {
+                console.log(response);
+                dispatch(authSucess(response.data));
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(authFail(err));
+            })
+        ;
     };
 };
